5b8b95126e75f4cc2777985d4b184df162f8298f
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-03-31 00:00:22 +0600

    Admin added

--- src/main/java/project2/Main.java
+++ src/main/java/project2/Main.java
@@ -5,12 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.SortedMap;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FileCreate file_create = new FileCreate();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginMenu.fxml"));
+        System.out.println("Check logic(): " + file_create.logic());
         if(file_create.logic()){
             fxmlLoader = new FXMLLoader(Main.class.getResource("NewsMenuBar.fxml"));
         }
