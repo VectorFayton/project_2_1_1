5f0182ddf673ed23aeb3c67e0ed923618f3bc6f0
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2023-03-30 19:53:21 +0600

    News

--- src/main/java/project2/NewsMenuBar.java
+++ src/main/java/project2/NewsMenuBar.java
@@ -1,4 +1,29 @@
 package project2;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class NewsMenuBar {
+    @FXML
+    Label user_avatar_label;
+    @FXML
+    Button technology_button;
+    @FXML
+    Button sports_button;
+    @FXML
+    Button business_button;
+    @FXML
+    Button economy_button;
+    @FXML
+    ImageView exit_image_button;
+
+    @FXML
+    protected void onExitButtonClick(){
+        user_avatar_label.getScene().getWindow().hide();
+        LoginMenu.OpenScene("LoginMenu");
+    }
+
 }
